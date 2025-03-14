e5e61e44a0100ba062c5ad72ce041df3467237c4
Mehdi Belhadi <mehdi.belhadi@icloud.com>
2025-03-14 17:16:36 +0100

    Final version

--- src/hooks/useApartment.jsx
+++ src/hooks/useApartment.jsx
@@ -6,10 +6,9 @@ export function useApartment() {
   const location = useLocation();
 
   useEffect(() => {
-    // Vérifie si location.state et location.state.apartmentId existent
     if (!location.state || !location.state.apartmentId) {
       console.error("Apartment ID is missing in location state");
-      return; // Si pas de `apartmentId`, on ne fait pas la requête
+      return; 
     }
 
     const abortController = new AbortController();
@@ -18,25 +17,23 @@ export function useApartment() {
     fetch("db.json", { signal })
       .then((res) => res.json())
       .then((flats) => {
-        // On cherche l'appartement avec l'ID passé dans l'état
         const flat = flats.find((flat) => flat.id === location.state.apartmentId);
         if (flat) {
-          setFlat(flat); // Si l'appartement existe, on le met dans l'état
+          setFlat(flat); 
         } else {
           console.error("Apartment not found");
         }
       })
       .catch((error) => {
         if (error.name !== "AbortError") {
-          console.error("Fetch error:", error); // Log uniquement les erreurs autres que AbortError
+          console.error("Fetch error:", error);
         }
       });
 
-    // Nettoyage de l'effet (annuler la requête si le composant est démonté)
     return () => {
       abortController.abort();
     };
-  }, [location.state]); // Ajoute location.state dans les dépendances pour que l'effet soit déclenché à chaque changement de `location`
+  }, [location.state]);
 
   return flat;
 }
